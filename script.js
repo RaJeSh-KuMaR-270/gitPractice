d8665bf672d9f671444b36065ede2d0630f3db37
RajeshKumar Perumal <rperumal270@everestdx.com>
2024-07-18 15:39:02 +0530

    modified greet2 to accept user name and greet

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function greet() {
-  console.log("Hello User")
+  console.log("Hello User");
   console.log("Have a nice day");
 }
 greet();
 
-function greet2() {
-  console.log("Welcome User")
-  console.log("Have a wonderfull day");
+function greet2(userName) {
+  if (user) {
+    console.log(`Welcome ${userName}`);
+    console.log(`Have a wonderfull day${userName}`);
+  } else console.log("Welcome User");
 }
 
-greet2();
+greet2("rajesh");
